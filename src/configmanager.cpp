e9e88a9f16ba5648ad1f727cad867d54dfe76de9
soyfabi <fabi.marzan1762@yahoo.com>
2023-05-14 12:40:09 -0400

    Add getGlobalFloat and getFloat

--- src/configmanager.cpp
+++ src/configmanager.cpp
@@ -87,6 +87,19 @@ bool getGlobalBoolean(lua_State* L, const char* identifier, const bool defaultVa
 	return val != 0;
 }
 
+float getGlobalFloat(lua_State* L, const char* identifier, const float defaultValue = 0.0f)
+{
+	lua_getglobal(L, identifier);
+	if (!lua_isnumber(L, -1)) {
+		lua_pop(L, 1);
+		return defaultValue;
+	}
+
+	float val = lua_tonumber(L, -1);
+	lua_pop(L, 1);
+	return val;
+}
+
 }
 
 ConfigManager::ConfigManager()
@@ -357,6 +370,15 @@ bool ConfigManager::getBoolean(boolean_config_t what) const
 		return false;
 	}
 	return boolean[what];
+}
+
+float ConfigManager::getFloat(floating_config_t what) const
+{
+	if (what >= LAST_FLOATING_CONFIG) {
+		std::cout << "[Warning - ConfigManager::getFloat] Accessing invalid index: " << what << std::endl;
+		return 0.0f;
+	}
+	return floating[what];
 }
 
 float ConfigManager::getExperienceStage(uint32_t level) const
