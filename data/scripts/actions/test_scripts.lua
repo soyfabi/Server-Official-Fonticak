20b59c31af5974417578441efa2e61d7554dd650
Fabian Marzan <fabi.marzan1762@yahoo.com>
2023-08-16 18:02:06 -0400

    Added Shop

--- data/scripts/actions/test_scripts.lua
+++ data/scripts/actions/test_scripts.lua
@@ -2,41 +2,40 @@ local action = Action()
 
 function action.onUse(player, item, fromPosition, target, toPosition, monster, isHotkey)
 
-<<<<<<< HEAD
-	local inbox = player:getInbox()
-	local parcel = Game.createItem(3504, 1)
+
+	local randomText = {
+		"#3545",
+		"#4627",
+		"#7236",
+		"#8267",
+		"#1477",
+		"#8937",
+		"#9316",
+		"#2151",
+		"#6732",
+		"#8783",
+		"#2346",
+		"#1572",
+		"#7893",
+		"#9435",
+		"#4687",
+		"#5272"
+	}
+	
+	local currentTime = os.time() -- Obtiene el tiempo actual en segundos desde el epoch (01/01/1970)
+	local currentMonth = os.date("%m", currentTime)
+	local currentDay = os.date("%d", currentTime) -- Obtiene el día del mes en formato de dos dígitos
+	local currentHour = os.date("%H:%M:%S", currentTime) -- Obtiene la hora actual en formato HH:MM:SS
+	local randomIndex = math.random(1, #randomText)
+	local selectedText = randomText[randomIndex] -- Obtener el texto aleatorio seleccionado
+	
 	local letter = Game.createItem(3506, 1)
+	local text_letter = ("                  <-Fonticak Receipt->\n-----------------------------\nOrder: "..selectedText.."\nFrom: "..player:getName().."\nDate: "..currentDay.." - "..currentMonth.." - Time: "..currentHour.."\n-----------------------------\nQTY                        ITEM                       COST\n-----------------------------\nHola\n\n  <~-~-~-!--!-------!--!-~-~-~>")
 	
-	local text_letter = [[
-                   ( Fonticak Shopper )
------------------------------
-Thank you for buying with us, your product has a 1-year warranty so do not throw away this letter to claim again.
-
-Visit us on the website to see more products.
-]]
-
-	
-	
-	--ITEM_LETTER_STAMPED
-	
-	if inbox then
-		parcel:addItemEx(letter, 1, INDEX_WHEREEVER, FLAG_NOLIMIT)
-		inbox:addItemEx(parcel, 1, INDEX_WHEREEVER, FLAG_NOLIMIT)
-		letter:setAttribute(ITEM_ATTRIBUTE_TEXT, text_letter)	
-	end
-	
-	
-	
-	
-	
-	
+	player:addItemEx(letter, 1)
+	letter:setAttribute(ITEM_ATTRIBUTE_TEXT, text_letter)
 	player:say("hola")
 	
-=======
-	player:say("hola")
-	
-	
->>>>>>> a3a218a164dba496a0192e2afd4650761fdf7e28
 	return true
 end
 
