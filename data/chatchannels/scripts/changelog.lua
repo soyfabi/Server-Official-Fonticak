32669cb51a3332b632bed624bf919eddb9536a69
Fabian Marzan <fabi.marzan1762@yahoo.com>
2023-08-24 14:19:11 -0400

    Fix Channels #1

--- data/chatchannels/scripts/changelog.lua
+++ data/chatchannels/scripts/changelog.lua
@@ -1,22 +1,13 @@
-function onSpeak(player, type, message)
-	local playerAccountType = player:getAccountType()
-	if player:getLevel() < 20 then
-		player:sendCancelMessage("You may not speak into channels as long as you are on level 20.")
-		return false
-	end
+function canJoin(player)
+	return player:getVocation():getId() ~= VOCATION_NONE or player:getAccountType() >= ACCOUNT_TYPE_SENIORTUTOR
+end
 
-	if type == TALKTYPE_CHANNEL_Y then
-		if playerAccountType >= ACCOUNT_TYPE_GAMEMASTER then
-			type = TALKTYPE_CHANNEL_Y
+function onSpeak(player, type, message)
+	if player:getAccountType() >= ACCOUNT_TYPE_GAMEMASTER then
+		if type == TALKTYPE_CHANNEL_Y then
+			return TALKTYPE_CHANNEL_O
 		end
-	elseif type == TALKTYPE_CHANNEL_O then
-		if playerAccountType < ACCOUNT_TYPE_GAMEMASTER then
-			type = TALKTYPE_CHANNEL_Y
-		end
-	elseif type == TALKTYPE_CHANNEL_R1 then
-		if playerAccountType < ACCOUNT_TYPE_GAMEMASTER and not player:hasFlag(PlayerFlag_CanTalkRedChannel) then
-			type = TALKTYPE_CHANNEL_Y
-		end
+		return true
 	end
-	return type
+	return false
 end
